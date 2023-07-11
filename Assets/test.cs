b83edf03aac2284fe9ac04be7dd31eafd0d158f7
AnJaeHo <era52001@gmail.com>
2023-07-11 19:11:09 +0900

    ..

--- Assets/test.cs
+++ Assets/test.cs
@@ -1,19 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.UI;
 
 public class test : MonoBehaviour
 {
-    public Material material;
-    // Start is called before the first frame update
-    void Start()
+
+    public Tilemap map;
+    public TileBase tile;
+
+    private void Start()
     {
-        material = GetComponent<Material>();
-        
+        FinishedBruning(new Vector3Int(0, 0, 0));
     }
 
-    // Update is called once per frame
-    void Update()
+    public void FinishedBruning(Vector3Int position)
+    {
+        map.SetTile(position, tile);
+        map.RefreshAllTiles();
+    }
+
+    public void TryToSpread(Vector3Int position,float spreadChance)
     {
         
     }
